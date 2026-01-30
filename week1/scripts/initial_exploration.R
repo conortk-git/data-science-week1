02f25ba10a38559e1cce2cd4a23bec0288fbdf78
conortk-git <ybz24cau@UEA.AC.UK>
2026-01-30 14:11:13 +0000

    added data set and explored the data maiing initial comments

---
+++ week1/scripts/initial_exploration.R
@@ -0,0 +1,42 @@
+# Week 1: Initial Data Exploration ====
+# Author: conor toms-kelly
+# Date: 30/01/26
+
+# Load packages ====
+library(tidyverse)
+library(here)
+library(naniar)
+library(janitor)
+library(skimr)
+# Load data ====
+mosquito_egg_raw <- read_csv(here("week1", "data", "mosquito_egg_data.csv"),
+                             name_repair = janitor::make_clean_names)
+
+# Basic overview ====
+glimpse(mosquito_egg_raw)
+summary(mosquito_egg_raw)
+skim(mosquito_egg_raw)
+
+# React table====
+# view interactive table of data
+view(mosquito_egg_raw)
+
+
+# Counts by site and treatment====
+
+mosquito_egg_raw |> 
+  group_by(site, treatment) |> 
+  summarise(n = n())
+
+# Observations ====
+# Your observations (add as comments below):
+# - What biological system is this?
+#   observing mosquito reproduction and the effects of pesticides on how many eggs hatch out of those laid
+# - What's being measured?
+#   how many of the eggs that are laid end up hatching 
+# - How many observations?
+#   205 observations
+# - Anything surprising?
+#   one of the rows (female 23) laid less eggs then was hatched 
+# - Any obvious problems?
+#   N/As, misspelt columns, collector names wrong, and what was mentioned in the thing that surprised me.
